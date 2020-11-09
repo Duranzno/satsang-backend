4e4a054b4af37156a30d7cc0f6d78a37211943fa
Alejandro D <aledurax@gmail.com>
2020-11-09 09:25:31 -0400

    fix(wip) heroku crash h10

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import dotenv from "dotenv"
 
 import app from "./app"
-import { APP_PORT } from "./utilities/secrets"
+import { PORT } from "./utilities/secrets"
 import logger from "./utilities/logger"
 
 dotenv.config()
 app
-  .listen(APP_PORT, () => {
-    logger.info(`server running on port : ${APP_PORT}`)
-    console.log(`server running on port : ${APP_PORT}`)
+  .listen(PORT, () => {
+    logger.info(`server running on port : ${PORT}`)
+    console.log(`server running on port : ${PORT}`)
   })
   .on("error", (e) => logger.error(e))
