64e03ea221c80a72bd5da4e969292fe29fc22f2e
Alejandro D <aledurax@gmail.com>
2020-11-13 18:57:25 -0400

    fix: fixed heroku deployment

--- src/middleware/apiDocs.ts
+++ src/middleware/apiDocs.ts
@@ -1,22 +1,36 @@
 import path from "path"
+import fs from 'fs'
 
 import { Router } from "express"
 import YAML from "yamljs"
 import swaggerUi from "swagger-ui-express"
 
 import { IS_PRODUCTION } from "../utilities"
-
+import logger from "../utilities/logger"
+export const getYamlFile = (): null | swaggerUi.JsonObject => {
+  let swaggerSpec: swaggerUi.JsonObject = {};
+  const specLocation = path.join(__dirname, "..", "openapi.yaml")
+  if (fs.existsSync(specLocation)) {
+    swaggerSpec = YAML.load(specLocation)
+    return swaggerSpec
+  }
+  return null
+}
 export const handleAPIDocs = (router: Router) => {
   const options: swaggerUi.SwaggerUiOptions = {}
-  const swaggerSpec = YAML.load(path.join(__dirname, "..", "openapi.yaml"))
-  swaggerSpec.host = "localhost:3000"
-  swaggerSpec.info.version = `${process.env.npm_package_version}`
-  if (!IS_PRODUCTION) {
-    swaggerSpec.servers.unshift({
-      url: "http://localhost:3000/",
-      description: "Localhost, only for development",
-    })
+  const swaggerSpec = getYamlFile()
+  if (swaggerSpec) {
+    swaggerSpec.host = "localhost:3000"
+    swaggerSpec.info.version = `${process.env.npm_package_version}`
+    if (!IS_PRODUCTION) {
+      swaggerSpec.servers.unshift({
+        url: "http://localhost:3000/",
+        description: "Localhost, only for development",
+      })
+    }
+    router.use("/", swaggerUi.serve)
+    router.get("/", swaggerUi.setup(swaggerSpec, options))
+  } else {
+    logger.error("Error loading OpenAPI Spec File")
   }
-  router.use("/", swaggerUi.serve)
-  router.get("/", swaggerUi.setup(swaggerSpec, options))
 }
